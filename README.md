f405de9b13955b474626a8e5d369d8a37bd38bc5
Willie Kwok <willie.kc.kwok@gmail.com>
2014-02-23 13:33:34 -0500

    final version

---
+++ README.md
@@ -0,0 +1,3 @@
+TO build to heroku:
+heroku create --stack cedar --buildpack https://github.com/oortcloud/heroku-buildpack-meteorite.git
+
--- leaderboard.js
+++ leaderboard.js
@@ -20,6 +20,10 @@ if (Meteor.isClient) {
     return Session.get("time");
   };
 
+  Template.progress.time = function () {
+    return ((Session.get("time")*10)/20)*100;
+  };
+
   Template.leaderboard.players = function () {
     return Players.find({});
   };
@@ -84,7 +88,11 @@ Template.moveTime.helpers({
 });
 
  Template.moveList.moves = function () {
-    return Moves.find({}, {sort: {date_created: -1}, limit: 10});
+      if( /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        return false
+      } else{
+        return Moves.find({}, {sort: {date_created: -1}, limit: 10});
+      }
   };
 
 }
@@ -101,48 +109,58 @@ if (Meteor.isServer) {
         Players.insert({name: names[i], score: 0});
     }
 
-    Instruction.insert({move: 0, go: false});
+    Instruction.insert({tag: "1", move: 0, go: false});
 
     collectionApi = new CollectionAPI({ authToken: '97f0ad9e24ca5e0408a269748d7fe0a0' });
     collectionApi.addCollection(Instruction, 'instruction');
     collectionApi.start();
 
   });
+
+  function moveCar(move){
+    console.log("Move called");
+    HTTP.call("GET", "http://30e0f157.ngrok.com/"+(move+10));
+  }
+
   Meteor.setInterval(function(){
     //Update Latest Move
-    //Moves.update  
-
+    //Moves.update
 
     topPlayer = Players.findOne({}, {sort: {score: -1, name: 1}})
     console.log(topPlayer);
     if (topPlayer.score == 0){
-      Instruction.update( {}, {move: 0, go: true}); 
+      Instruction.update( {}, {move: 0, go: true});
       Meteor.setTimeout(function(){
-              Instruction.update( {}, {move: 0, go: false});     
-      }, 250);    
+          Instruction.update( {}, {move: 0, go: false});     
+      }, 250);  
+      moveCar(0);
     }
     else if (topPlayer.name == "Backward"){
       Instruction.update( {}, {move: 1, go: true});
+      moveCar(1);
       Meteor.setTimeout(function(){
-              Instruction.update( {}, {move: 0, go: false});     
+          Instruction.update( {}, {move: 1, go: false});     
       }, 250);         
     }
     else if (topPlayer.name == "Forward"){
       Instruction.update( {}, {move: 2, go: true}); 
+      moveCar(2);
       Meteor.setTimeout(function(){
-              Instruction.update( {}, {move: 0, go: false});     
+         Instruction.update( {}, {move: 2, go: false});     
       }, 250);    
     }
     else if (topPlayer.name == "Turn Left"){
       Instruction.update( {}, {move: 3, go: true});
+      moveCar(3);
       Meteor.setTimeout(function(){
-              Instruction.update( {}, {move: 0, go: false});     
+          Instruction.update( {}, {move: 3, go: false});     
       }, 250);     
     }
     else if (topPlayer.name == "Turn Right"){
       Instruction.update( {}, {move: 4, go: true});
+      moveCar(4);
       Meteor.setTimeout(function(){
-              Instruction.update( {}, {move: 0, go: false});     
+          Instruction.update( {}, {move: 4, go: false});     
       }, 250);    
     }
 
@@ -158,20 +176,19 @@ if (Meteor.isServer) {
 
 
     if (Moves.find().count() > 10){
-      console.log("HIII")
       lastMove = Moves.findOne({}, {sort: {date_created: 1}})
       Moves.remove({date_created: lastMove.date_created})
     }
-  }, 5000);
+  }, 3000);
 
-  var clock = 4;
+  var clock = 2;
   Meteor.setInterval(function () {
       clock -= 1;
       // end of game
       console.log(clock);
       if (clock === -1) {
         // stop the clock
-        clock = 4;
+        clock = 2;
       }
 
     }, 1000);
